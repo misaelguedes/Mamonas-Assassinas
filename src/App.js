67f0cd6ca97c85c160416077cf4a954c80858fde
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-10 20:10:30 -0300

    Criando Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter} from 'react-router-dom'
 
 import RoutesApp from './Routes'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header/>
       <RoutesApp/>
+      <Footer/>
     </BrowserRouter>
   )
 }
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/misaelguedes" target="blank">&reg; Misael Guedes</a>
+        </footer>
+    )
+}
