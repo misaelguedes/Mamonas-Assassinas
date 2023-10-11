e3ff5d5cbad3dc53c12c621d195f00a5f6fb5ec5
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-11 11:33:39 -0300

    Criando responsividade do site

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 import './Header.css'
@@ -5,14 +6,44 @@ import './Header.css'
 import capa from '../../Assets/capa.png'
 
 export default function Header() {
+
+    const [itensVisible, setItensVisible] = useState(false)
+
+    useEffect(() => {
+        function mudouTamanho() {
+            setItensVisible(window.innerWidth >= 768)
+        }
+
+        window.addEventListener('resize', mudouTamanho);
+
+        mudouTamanho();
+
+        return () => {
+            window.removeEventListener('resize', mudouTamanho);
+          };
+    }, [])
+    
+    function clickMenu() {
+        setItensVisible(!itensVisible)
+    }
+
+    function hideMenu() {
+        if (window.innerWidth <= 768) {
+            setItensVisible(false)
+        }
+    }
+
     return (
         <header>
             <img src={capa} alt="Mamonas Assassinas"/>
             <div className="itens">
-                <Link to='/'>Home</Link>
-                <Link to='/historia'>História</Link>
-                <Link to='/discografia'>Discografia</Link>
-                <Link to='/premios'>Prêmios</Link>
+                <span id="burguer" class="material-symbols-outlined" onClick={clickMenu}>menu</span>
+                <menu className="menu" style={{ display: itensVisible ? 'block' : 'none' }}>
+                    <Link to='/' onClick={hideMenu}>Home</Link>
+                    <Link to='/historia' onClick={hideMenu}>História</Link>
+                    <Link to='/discografia' onClick={hideMenu}>Discografia</Link>
+                    <Link to='/premios' onClick={hideMenu}>Prêmios</Link>
+                </menu>
             </div>
         </header>
     )
