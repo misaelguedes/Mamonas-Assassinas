53c27a69e72cbbd0d5318bc8fc8e38b74d9301d5
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-10 22:21:07 -0300

    Criando a página História

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 
 import Home from '../Pages/Home'
+import Historia from '../Pages/Historia'
 import Discografia from '../Pages/Discografia'
 import Premios from '../Pages/Premios'
 
@@ -8,6 +9,7 @@ export default function RoutesApp() {
     return (
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/historia' element={<Historia/>}/>
             <Route path='/discografia' element={<Discografia/>}/>
             <Route path='/premios' element={<Premios/>}/>
         </Routes>
