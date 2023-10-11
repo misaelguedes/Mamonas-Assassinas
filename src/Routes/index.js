4d1df3be977cd469f4526def4450865635b3e974
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-10 21:58:00 -0300

    Criando a página Prêmios

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,14 @@ import {Routes, Route} from 'react-router-dom'
 
 import Home from '../Pages/Home'
 import Discografia from '../Pages/Discografia'
+import Premios from '../Pages/Premios'
 
 export default function RoutesApp() {
     return (
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/discografia' element={<Discografia/>}/>
+            <Route path='/premios' element={<Premios/>}/>
         </Routes>
     )
 }
